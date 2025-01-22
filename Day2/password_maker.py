79789c683d53ace80e7fa9caac75c4299c1014dd
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-01-22 23:47:47 +0100

    Day2

---
+++ Day2/password_maker.py
@@ -0,0 +1,10 @@
+import random
+caracters = {
+    'words': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    'special_caracters': ['!', '@', '#', '$', '%', '&', '/']
+    }
+
+def password_maker(upper_words:str, number:str, special_caracters:str, len:int) -> str:
+    random.randint()
+    
